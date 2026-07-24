b4b421a9b50881f1f4a34f058f7605d51872cf96
kdev_sobreira <kauam.morais09@gmail.com>
2026-07-24 20:39:08 -0300

    feat(db): adiciona migration inicial (tabelas User e Listing)

---
+++ backend/prisma/migrations/20260715232934_init/migration.sql
@@ -0,0 +1,37 @@
+-- CreateEnum
+CREATE TYPE "Category" AS ENUM ('BOOKS', 'STUDY_MATERIALS', 'ELECTRONICS', 'EQUIPMENT', 'FURNITURE', 'CLOTHING', 'ACCESSORIES', 'OTHER');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "avatar" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Listing" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" DECIMAL(65,30),
+    "isDonation" BOOLEAN NOT NULL DEFAULT false,
+    "imageUrl" TEXT NOT NULL,
+    "category" "Category" NOT NULL,
+    "sellerId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Listing_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Listing" ADD CONSTRAINT "Listing_sellerId_fkey" FOREIGN KEY ("sellerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
