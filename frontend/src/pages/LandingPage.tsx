60a14910ad95a40915de9c9cb687c2ff882c2560
kdev_sobreira <kauam.morais09@gmail.com>
2026-07-29 00:00:43 -0300

    fix(layout): Organizando onde footer aparece

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,11 +1,14 @@
 import { Hero } from "../components/landing/Hero";
+import { Footer } from "../components/layout/Footer";
 import { FeaturedListings } from "../components/landing/FeaturedListings";
+
 
 export function LandingPage() {
   return (
     <div>
       <Hero />
       <FeaturedListings />
+      <Footer />
     </div>
   );
 }
