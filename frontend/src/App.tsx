218d139779c56d87c3cb909af4aaa114a2710380
kdev_sobreira <kauam.morais09@gmail.com>
2026-07-29 00:08:57 -0300

    refactor(frontend): ajustando rotas finais do projeto

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,31 @@
+// App.tsx
 import { Routes, Route } from "react-router-dom";
-import { Navbar } from "./components/layout/Navbar";
-import { Footer } from "./components/layout/Footer";
+import { MainLayout } from "./components/layout/MainLayout";
+import { BareLayout } from "./components/layout/BareLayout";
 import { LandingPage } from "./pages/LandingPage";
 import { MarketplacePage } from "./pages/MarketplacePage";
-import {ListingDetailPage} from "./pages/ListingDetailPage"
-import { CreateListingPage } from "./pages/CreateListingPAge"
+import { ListingDetailPage } from "./pages/ListingDetailPage";
+import { CreateListingPage } from "./pages/CreateListingPage";
+import { DashboardPage } from "./pages/DashboardPage";
+import { AuthPage } from "./pages/AuthPage";
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <main className="pt-16">
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/explorar" element={<MarketplacePage />} />
-          <Route path="/anuncios/:id" element={<ListingDetailPage />} />
-          <Route path="/anuncios/novo" element={<CreateListingPage />} />
-        </Routes>
-      </main>
-      <Footer />
-    </>
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/explorar" element={<MarketplacePage />} />
+        <Route path="/anuncios/:id" element={<ListingDetailPage />} />
+        <Route path="/anuncios/novo" element={<CreateListingPage />} />
+        <Route path="/anuncios/:id/editar" element={<CreateListingPage />} />
+        <Route path="/meus-anuncios" element={<DashboardPage />} />
+      </Route>
+
+      <Route element={<BareLayout />}>
+        <Route path="/entrar" element={<AuthPage />} />
+        <Route path="/cadastro" element={<AuthPage />} />
+      </Route>
+    </Routes>
   );
 }
 
